d778fcd7d43321ac1b57e7264f4c763a758bf415
LeonidSennik <barodfsh@gmail.com>
2025-06-15 15:43:59 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '50868712-4e51c6fc19c53f8b926e21bf1'; // заміни на свій ключ
+const API_KEY = '50868712-4e51c6fc19c53f8b926e21bf1'; 
 
 export async function getImagesByQuery(query) {
   const response = await axios.get(BASE_URL, {
